4e1fa390ea0b6951159188dba3bc12fde50c1efe
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2020-10-28 11:56:06 -0300

    Third day update
    Reproduzindo vídeo buffers com Media Source.

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -1,13 +1,20 @@
+const MANIFEST_URL = 'manifest.json';
+const localHost = ['127.0.0.1', 'localhost']
+
 async function main() {
-    const player = videojs('vid');
-    const ModalDialog = videojs.getComponent('ModalDialog');
-    const modal = new ModalDialog(player, {
-        temporary: false, 
-        closeable: true
-    });
+  const isLocal = localHost.includes(window.location.hostname)
+  //console.log('isLocal? ' + isLocal);
 
-    player.addChild(modal);
+  const manifestJSON = await (await fetch(MANIFEST_URL)).json();
+  const host = isLocal ? manifestJSON.localHost : manifestJSON.productionHost;
+  //console.log('host?', host);
 
+  const videoComponent = new VideoComponent();
+  const network = new Network({ host })
+  const videoPLayer = new VideoMediaPlayer({ manifestJSON, network });
+  
+  videoPLayer.initializeCodec()
+  videoComponent.initializerPlayer();
 }
 
 window.onload = main
